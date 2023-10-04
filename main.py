82dc260f5a7b951c03970cd9866824d72fb117f6
AtsushiUtsumi <debtgx103@gmail.com>
2023-10-04 22:08:02 +0900

    コメント追記

--- main.py
+++ main.py
@@ -17,4 +17,9 @@ entity.application('py')
 
 # print('sugumi起動。本日目標はアプリケーション層の起動のみ')
 import usecase
-usecase.presentation('flask', 'py')
+#usecase.presentation('flask', 'py')
+
+
+# 思ったんだが「このツール自体がDDDで書かれていない」、というのが引っかかる...
+# この「sugumi」自体はinfrastructure側の技術なのかも。
+# だとしたら単体のアプリケーションといううよりはDBのドライバライブラリのような位置付けで作成した方がいいのかも
